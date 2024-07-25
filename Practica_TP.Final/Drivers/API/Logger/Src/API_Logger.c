611d3df82eb77f3034fc66d24ebf560903cd6f78
RobAxt <roberto.axt@gmail.com>
2024-07-25 18:45:56 -0300

    comment logger function

--- Practica_TP.Final/Drivers/API/Logger/Src/API_Logger.c
+++ Practica_TP.Final/Drivers/API/Logger/Src/API_Logger.c
@@ -14,7 +14,8 @@
 
 /* Private define ------------------------------------------------------------*/
 
-#define LINEsIZE    64
+#define LINEsIZE       64
+#define LOGfORMAT      16
 #define DEBUGlEVEL    "DBG"
 #define WARNINGlEVEL  "WAR"
 #define ERRORlEVEL    "ERR"
@@ -30,36 +31,36 @@ void API_Logger_Log(uint8_t *level, uint8_t *string, uint8_t size);
 /* API code ------------------------------------------------------------------*/
 
 /**
-  * @brief  ...
-  * @param  ...
-  * @retval ...
+  * @brief  Log string in Debug level.
+  * @param  uint8_t *string: null terminated char array with the log message.
+  * @retval None.
   */
 void API_Logger_LogDebug(uint8_t *string)
 {
   if(NULL != string)
-    API_Logger_Log((uint8_t*) DEBUGlEVEL, string, strlen((char*)string));
+    API_Logger_Log((uint8_t*) DEBUGlEVEL, string, strlen((char*)string)+strlen(DEBUGlEVEL));
 }
 
 /**
-  * @brief  ...
-  * @param  ...
-  * @retval ...
+  * @brief  Log string in Warning level.
+  * @param  uint8_t *string: null terminated char array with the log message.
+  * @retval None.
   */
 void API_Logger_LogWarning(uint8_t *string)
 {
   if(NULL != string)
-    API_Logger_Log((uint8_t*) WARNINGlEVEL, string, strlen((char*)string));
+    API_Logger_Log((uint8_t*) WARNINGlEVEL, string, strlen((char*)string)+strlen(WARNINGlEVEL));
 }
 
 /**
-  * @brief  ...
-  * @param  ...
-  * @retval ...
+  * @brief  Log string in Error level.
+  * @param  uint8_t *string: null terminated char array with the log message.
+  * @retval None.
   */
 void API_Logger_LogError(uint8_t *string)
 {
   if(NULL != string)
-    API_Logger_Log((uint8_t*) ERRORlEVEL, string, strlen((char*)string));
+    API_Logger_Log((uint8_t*) ERRORlEVEL, string, strlen((char*)string)+strlen(ERRORlEVEL));
 }
 
 /*----------------------------------------------------------------------------*/
@@ -67,19 +68,19 @@ void API_Logger_LogError(uint8_t *string)
 /* Private API code ----------------------------------------------------------*/
 
 /**
-  * @brief  ...
-  * @param  ...
-  * @retval ...
+  * @brief  Log string with custom formatting.
+  * @param  uint8_t *level: null terminated char array with the level message.
+  * @param  uint8_t *string: null terminated char array with the log message.
+  * @retval None.
   */
 void API_Logger_Log(uint8_t *level, uint8_t *string, uint8_t size)
 {
   uint8_t logString[LINEsIZE] = {0};
-  if(NULL != level && NULL != string && LINEsIZE > size)
+  if(NULL != level && NULL != string && LINEsIZE > (size + LOGfORMAT))
   {
     sprintf((char*)logString, "%08ld - %s - %s\r\n", API_Logger_HAL_Time(), (char *)level, (char *)string);
     API_Logger_HAL_UARTTx(logString, strlen((char*)logString));
   }
 }
-
 
 /*----------------------------------------------------------------------------*/
